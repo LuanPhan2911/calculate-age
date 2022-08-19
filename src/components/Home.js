e365a7ed9625f58f12c0f99a29bcbefaa6eed52f
luanphan2911 <baoluan291103oe@gmail.com>
2022-08-19 13:42:17 +0700

    calculator-age

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,87 @@
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import DatePicker from "react-datepicker";
-import TimePicker from 'react-time-picker';
 import { dateFormat, language } from "../utils/constant";
-
+import { FormattedMessage } from 'react-intl';
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 function Home() {
+    // function usePrevious(value) {
+    //     const ref = useRef();
+    //     useEffect(() => {
+    //         ref.current = value;
+    //     });
+    //     return ref.current;
+    // }
+
     const [startDate, setStartDate] = useState(moment().startOf('day').valueOf());
-    const [hour, setHour] = useState('00:00:00');
-    const [showSelectHour, setShowSelectHour] = useState(false);
+    const currentDate = moment().startOf('day').valueOf();
+    const [age, setAge] = useState({
+        years: 0,
+        months: 0,
+        days: 0,
+    });
     const lang = useSelector((state) => state.app.language);
 
-    console.log(moment(startDate).format('DD/MM/yyyy'));
+    if (startDate) {
 
+    }
+    useEffect(() => {
+        if (!startDate) {
+            setAge({
+                years: 0,
+                months: 0,
+                days: 0,
+            })
+        }
+        let idTime = setTimeout(() => {
+            let years = moment(currentDate).diff(moment(startDate), 'years');
+            let months = moment(currentDate).diff(moment(startDate), 'months');
+            let days = moment(currentDate).diff(moment(startDate), 'days');
+            setAge({ ...age, years, months, days });
+        }, 1000);
+        return () => {
+            clearTimeout(idTime);
+        }
+    }, [startDate]);
     return (
 
         <div className="container">
             <div className="card">
                 <div className="card-header">
-                    Welcome to Calculator Age
+                    <FormattedMessage id="menu.welcome" />
                 </div>
                 <div className="card-body">
                     <div className="form-group">
-                        <label>Choose your birth date</label>
+                        <label><FormattedMessage id="menu.select-age" /></label>
                         <DatePicker
                             selected={startDate}
                             onChange={(date) => setStartDate(moment(date).startOf('day').valueOf())}
                             dateFormat={lang === language.VI ? dateFormat.VI : dateFormat.EN}
                             maxDate={new Date()}
+                            placeholderText={lang === language.VI ? dateFormat.VI.toUpperCase() : dateFormat.EN.toUpperCase()}
                         />
                     </div>
-                    <div className="show-select-hour">
-                        {showSelectHour === false && <h6>
-                            If you know exact time your birth date,click here to select time➡️
+                    <div className="calculator-age">
 
+                        <h5>
+                            <FormattedMessage id="calculator-age.content" />
+                            {age.years}
+                            <FormattedMessage id="calculator-age.year" /> .
+                        </h5>
+                        <h5>
+                            <FormattedMessage id="calculator-age.content2" />
+                            {age.months}
+                            <FormattedMessage id="calculator-age.month" />
+                        </h5>
+                        <h5>
+                            <FormattedMessage id="calculator-age.content2" />
+                            {age.days}
+                            <FormattedMessage id="calculator-age.day" />
+                        </h5>
 
-                            <button
-                                className="btn btn-info"
-                                onClick={() => setShowSelectHour(true)}
-                            >Select Time</button>
-                        </h6>
-                        }
                     </div>
-                    <div className="select-time">
-                        <TimePicker onChange={setHour} value={hour} />
-                    </div>
-
                 </div>
             </div>
         </div>
